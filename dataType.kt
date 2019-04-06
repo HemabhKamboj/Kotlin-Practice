dd59eef3c7fbca10be46943acbb6027cd4e17040
Hemabh Kamboj <hkamboj2603199@gmail.com>
2019-04-06 19:18:28 +0530

    added dataypes

---
+++ dataType.kt
@@ -0,0 +1,27 @@
+/**
+ * You can edit, run, and share this code. 
+ * play.kotlinlang.org 
+ */
+
+fun main( args: Array<String>){
+    
+    var name: String
+    name = "Hemabh"
+    var age: Int = 10
+    var myAge = 10
+    var isAlive: Boolean = true 
+    var marks: Float = 97.4F
+    var percentage: Double = 90.78
+    var gender: Char = 'M'
+    
+    println("Marks")
+    println(name)
+    println(age)
+    println(isAlive)
+    println(myAge)
+    println(isAlive)
+    println(marks)
+    println(percentage)
+    println(gender)
+    
+}
